d6a486c47bf687f5740154f62caa3813674208cb
madhukarreddy326 <31158933+madhukarreddy326@users.noreply.github.com>
2019-04-07 15:28:18 +0530

    this is first commit related to designpatterns

---
+++ DesignPatterns/src/com/funnlearn/java/FirstSampleExample.java
@@ -0,0 +1,10 @@
+package com.funnlearn.java;
+
+public class FirstSampleExample {
+	
+	public static void main(String args[])
+	{
+		System.out.println("Helo: this is sampel first program to upload into github");
+	}
+
+}
